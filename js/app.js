1944517c6d3bb32b601552dc37e49abc8c1aa8db
Beltran Garcia Balbuena <beltrang@colbenson.com>
2018-12-13 22:23:49 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var slideLeft =$('.studies__column__one');
+    var slideRight =$('.studies__column__two');
+
+    slideLeft.addClass( "slide-out-left" );
+    slideRight.addClass( "slide-out-right" );
+
+    $(window).scroll(function (event) {
+         hT = slideLeft.offset().top,
+        hH = slideLeft.outerHeight(),
+        wheight = $(window).height(),
+        scroll = $(this).scrollTop();
+        if (scroll > (hT + hH - wheight)) {
+            slideLeft.removeClass("slide-out-left").addClass( "slide-in-left" );
+            slideRight.removeClass("slide-out-right").addClass( "slide-in-right" );
+        }
+    });
+  
+  });
+
